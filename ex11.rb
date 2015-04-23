ad2cf23291700538a2f8e71f5efecc5f7daa74e4
Inso-61 <damien.galicher@free.fr>
2015-04-23 18:47:19 +0200

    exoruby

---
+++ ex11.rb
@@ -0,0 +1,20 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+#gets.chomp permet de recuperer les valeurs qu'on a rentre
+
+puts "quel est ton nom?"
+nom = gets
+
+puts "mon nom est #{nom}"
+
+puts "quel est ton prenom?"
+prenom = gets.chomp
+
+puts "mon prenom est " + prenom
